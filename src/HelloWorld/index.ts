7f07c3ffb8d13e134ef75a5af83f9bbe821d8068
LordKenzo <lordkenzo@outlook.com>
2024-11-18 10:26:53 +0100

    first commit

---
+++ src/HelloWorld/index.ts
@@ -0,0 +1,15 @@
+import { app } from '@azure/functions';
+
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    route: 'v1/slack',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
