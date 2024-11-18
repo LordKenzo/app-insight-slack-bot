03b350fd743d8ff1319a06092732fc84edfcab09
LordKenzo <lordkenzo@outlook.com>
2024-11-18 14:52:57 +0100

    Change storage account name

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sa_bot" {
-  name                     = "sabot"
+  name                     = "lf-itn-sa-bot"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
