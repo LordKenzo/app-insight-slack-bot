fe11506198e462ddd809fa2aab38d3601f4079b6
LordKenzo <lordkenzo@outlook.com>
2024-11-18 23:49:41 +0100

    Fix plan

--- infra/kv.tf
+++ infra/kv.tf
@@ -12,3 +12,14 @@ data "azurerm_key_vault_secret" "appinsight_connection_string" {
   name         = "APPLICATIONINSIGHTS-CONNECTION-STRING"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+resource "azurerm_key_vault_access_policy" "terraform_sp" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  tenant_id    = var.tenant_id
+  object_id    = var.client_id    # Questo è l'object ID del service principal
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
