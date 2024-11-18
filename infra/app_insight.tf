f3b22636a3cb6669790840ae19281bb1209af831
LordKenzo <lordkenzo@outlook.com>
2024-11-18 20:10:21 +0100

    Add Workspace Analytics

--- infra/app_insight.tf
+++ infra/app_insight.tf
@@ -1,6 +1,15 @@
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "workspace-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "appinsights" {
   name                = "lf-itn-appinsights"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.workspace.id
   application_type    = "web"
 }
