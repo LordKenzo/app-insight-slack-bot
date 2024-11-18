10ad85028c2ab66bb28113e5a3a4eda003deae0e
LordKenzo <lordkenzo@outlook.com>
2024-11-18 15:03:50 +0100

    Fix deploy function

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,13 @@ terraform {
   }
 }
 
+backend "azurerm" {
+  resource_group_name  = "terraform-state-rg"
+  storage_account_name = "tfstateaccount"
+  container_name      = "tfstate"
+  key                 = "prod.terraform.tfstate"
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
